071bfbf5685c3344d359156856f79c0b2a6613c2
Tiago <tiagohslino@gmail.com>
2025-03-26 17:04:34 -0300

    add props and array at Suggestions.js

--- src/Suggestions.js
+++ src/Suggestions.js
@@ -1,70 +1,47 @@
 export default function Suggestions() {
+
+    const listaSugestoes = [
+        {imagem: "assets/img/bad.vibes.memes.svg", nome: "bad.vibes.memes", razao: "Segue você"},
+        {imagem: "assets/img/chibirdart.svg", nome: "chibirdart", razao: "Segue você"},
+        {imagem: "assets/img/razoesparaacreditar.svg", nome: "razoesparaacreditar", razao: "Novo no Instagram"},
+        {imagem: "assets/img/adorable_animals.svg", nome: "adorable_animals", razao: "Segue você"},
+        {imagem: "assets/img/smallcutecats.svg", nome: "smallcutecats", razao: "Segue você"},
+        
+    ];
+
     return (
+        
         <div class="sugestoes">
                 <div class="titulo">
                     Sugestões para você
                     <div>Ver tudo</div>
                 </div>
 
-                <div class="sugestao">
-                    <div class="usuario">
-                    <img src="assets/img/bad.vibes.memes.svg" alt="bad.vibes.memes.svg"/>
-                    <div class="texto">
-                        <div class="nome">bad.vibes.memes</div>
-                        <div class="razao">Segue você</div>
-                    </div>
-                    </div>
-
-                    <div class="seguir">Seguir</div>
-                </div>
-
-                <div class="sugestao">
-                    <div class="usuario">
-                    <img src="assets/img/chibirdart.svg" alt="chibirdart"/>
-                    <div class="texto">
-                        <div class="nome">chibirdart</div>
-                        <div class="razao">Segue você</div>
-                    </div>
-                    </div>
-
-                    <div class="seguir">Seguir</div>
-                </div>
-
-                <div class="sugestao">
-                    <div class="usuario">
-                    <img src="assets/img/razoesparaacreditar.svg" alt="razoesparaacreditar" />
-                    <div class="texto">
-                        <div class="nome">razoesparaacreditar</div>
-                        <div class="razao">Novo no Instagram</div>
-                    </div>
-                    </div>
-
-                    <div class="seguir">Seguir</div>
-                </div>
-
-                <div class="sugestao">
-                    <div class="usuario">
-                    <img src="assets/img/adorable_animals.svg" alt="adorable_animals"/>
-                    <div class="texto">
-                        <div class="nome">adorable_animals</div>
-                        <div class="razao">Segue você</div>
-                    </div>
-                    </div>
-
-                    <div class="seguir">Seguir</div>
-                </div>
-
-                <div class="sugestao">
-                    <div class="usuario">
-                    <img src="assets/img/smallcutecats.svg" alt="smallcutecats"/>
-                    <div class="texto">
-                        <div class="nome">smallcutecats</div>
-                        <div class="razao">Segue você</div>
-                    </div>
-                    </div>
-
-                    <div class="seguir">Seguir</div>
-                </div>
+            {listaSugestoes.map((item, index) => 
+                <Suggestion nome={item.nome} 
+                    imagem={item.imagem}
+                    razao={item.razao}
+                    key={index}
+                />)
+            }
+                
         </div>
+    );
+}
+
+function Suggestion({imagem, nome, razao}) {
+    
+    return (
+        <div class="sugestao">
+                <div class="usuario">
+                    <img src={imagem} alt={nome}/>
+                    <div class="texto">
+                        <div class="nome">{nome}</div>
+                        <div class="razao">{razao}</div>
+                    </div>
+                </div>
+
+                <div class="seguir">Seguir</div>
+            </div>
     );
 }
