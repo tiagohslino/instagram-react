cc9560407d255203fdcf76b886845409cae3609b
Tiago <tiagohslino@gmail.com>
2025-03-27 11:28:01 -0300

    change avatar and username js

--- src/User.js
+++ src/User.js
@@ -1,11 +1,28 @@
+import React from "react";
+
 export default function User() {
+
+    const imagemAvatar = "assets/img/catanacomics.svg";
+    const [nome, setNome] = React.useState("catanacomics");
+    const [avatar, setAvatar] = React.useState(imagemAvatar);
+
+    function mudarNome(){
+        const nomeDigitado = prompt("Digite seu nome de usuario");
+        setNome(nomeDigitado);
+    }
+
+    function mudarAvatar(){
+        const linkDigitado = prompt("Digite o link da sua imagem");
+        setAvatar(linkDigitado);
+    }
+
     return(
         <div class="usuario">
-            <img src="assets/img/catanacomics.svg" alt="imagem de perfil"/>
+            <img onClick={mudarAvatar} src={avatar} alt="imagem de perfil"/>
                 <div class="texto">
                     <span>
-                    <strong>catanacomics</strong>
-                    <ion-icon name="pencil"></ion-icon>
+                    <strong>{nome}</strong>
+                    <ion-icon onClick={mudarNome} name="pencil"></ion-icon>
                     </span>
                 </div>
         </div>
